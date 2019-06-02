33d4ef4270b482e39b4952405003659e85cc0fed
thompsonjonm <thompson.jonm@gmail.com>
2019-06-01 21:06:18 -0400

    Tests added

--- cypress/integration/calendars/calendars.js
+++ cypress/integration/calendars/calendars.js
@@ -1,1 +1,25 @@
-import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
+import {
+  Given,
+  When,
+  And,
+  Then
+} from 'cypress-cucumber-preprocessor/steps';
+
+/**
+ * Scenario: Calendars Indicate the Number of Trips they are Used By
+ */
+Given('we are viewing the "Trips" tab for a specific feed', () => {
+
+});
+
+When('we create a trip using an existing calendar', () => {
+
+});
+
+And('navigate to the "Calendars" tab', () => {
+
+});
+
+Then('the calendar should indicate trip usage', () => {
+
+})
