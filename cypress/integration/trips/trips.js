c3affe943b7ee62fafd99eaa2b6c9f8d0537bae6
thompsonjonm <thompson.jonm@gmail.com>
2019-06-01 21:06:18 -0400

    Tests added

--- cypress/integration/trips/trips.js
+++ cypress/integration/trips/trips.js
@@ -1,1 +1,28 @@
-import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
+import {
+  Given,
+  When,
+  And,
+  Then
+} from 'cypress-cucumber-preprocessor/steps';
+
+/**
+ * Scenario: Create a New Trip
+ *
+ * @param create_or_delete           The type of action we are taking
+ * @param created_or_deleted         The type of assertion we are passing
+ */
+Given('we are viewing the "Trips" tab for a specific feed', () => {
+
+});
+
+And('we have existing routes, patterns, and calendars', () => {
+
+});
+
+When('we select to {string} a trip', (create_or_delete) => {
+
+});
+
+Then('our trip should be successfully {string}', (created_or_deleted) => {
+
+})
