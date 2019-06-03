f61b1f7edb5827bf680a74dcccb64cae850891ba
thompsonjonm <thompson.jonm@gmail.com>
2019-06-02 20:54:27 -0400

    Creation of CalendarUtil and calendar test specs

--- cypress/integration/calendars/calendars.js
+++ cypress/integration/calendars/calendars.js
@@ -6,22 +6,53 @@ import {
 } from 'cypress-cucumber-preprocessor/steps';
 import * as CONSTANTS from '../../support/architect/constants';
 import LoginUtil from '../../support/architect/utilities/LoginUtil';
+import CalendarUtil from '../../support/architect/utilities/CalendarUtil'
 
 /**
  * Scenario: Calendars Indicate the Number of Trips they are Used By
  */
 Given('we are viewing the "Trips" tab for a specific feed', () => {
-  LoginUtil.loginAsArchitectUser()
+  LoginUtil.loginAsArchitectUser(CONSTANTS.ARCHITECT_USER, CONSTANTS.ARCHITECT_PASSWORD);
+
+  cy.request({
+    "method": "GET",
+    "url": CONSTANTS.API_URL + 'feeds',
+    "headers": {
+      "Authorization": CONSTANTS.API_TOKEN,
+      "Content-Type": "application/json"
+    }
+  }).then((response) => {
+    const feed = response.body[1].feed_id;
+
+    cy.get('[data-id=feed-' + feed + ']')
+      .click();
+  });
+
+  cy.get(CONSTANTS.FEED.TRIPS_SELECTOR)
+    .click();
 });
 
 When('we create a trip using an existing calendar', () => {
+  cy.url()
+    .should('include', 'trips');
 
+  cy.get(CONSTANTS.TRIPS.NEW_TRIP_SELECTOR)
+    .click();
+
+  cy.get(CONSTANTS.TRIPS.STOP_TIME_SELECTOR)
+  TODO Continue working on filling out time inputs
 });
 
 And('navigate to the "Calendars" tab', () => {
+  cy.get(CONSTANTS.NAVIGATION.FEED_LINK_SELECTOR)
+    .contains('Automation Testing Feed')
+    .click();
 
+  cy.get(CONSTANTS.FEED.CALENDARS)
+    .click();
 });
 
 Then('the calendar should indicate trip usage', () => {
-
+  cy.get(CONSTANTS.CALENDARS.TRIP_USAGE_SELECTOR)
+    .contains('1 trip')
 })
--- cypress/support/architect/utilities/LoginUtil.js
+++ cypress/support/architect/utilities/LoginUtil.js
@@ -1,27 +1,42 @@
 import * as CONSTANTS from '../constants'
 
 /**
-* Class for helper functions related to Architect login
-*/
+ * Class for helper functions related to Architect login
+ */
 export default class LoginUtil {
 
   /**
-  * A login function which takes a username and password
-  * to gain access to the Architect application
-  *
-  * @param username       The username to login with
-  * @param password       The password for the username
-  */
+   * A login function which takes a username and password
+   * to gain access to the Architect application
+   *
+   * @param username - The username to login with
+   * @param password - The password for the username
+   */
   static loginAsArchitectUser(username, password) {
     cy.visit(CONSTANTS.LOGIN_URL)
-    
+
     cy.get(CONSTANTS.LOGIN.USERNAME_FIELD_SELECTOR)
-      .type(CONSTANTS.ARCHITECT_USER);
+      .type(username);
 
     cy.get(CONSTANTS.LOGIN.PASSWORD_FIELD_SELECTOR)
-      .type(CONSTANTS.ARCHITECT_PASSWORD);
+      .type(password);
 
     cy.get(CONSTANTS.LOGIN.SUBMIT_BUTTON_SELECTOR)
       .click();
+  }
+
+  /**
+   * A login function which takes a username and password
+   * to gain access to the Architect application by hitting
+   * its login API for increased test speed
+   *
+   * @param username - The username to login with
+   * @param password - The password for the username
+   */
+  static loginAsArchitectUserWithAPI(username, password) {
+    cy.request('POST', CONSTANTS.LOGIN_URL, {
+      username: username,
+      password: password
+    });
   }
 }
